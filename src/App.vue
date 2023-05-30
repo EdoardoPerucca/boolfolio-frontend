ad4be834961b6065dabd54613b1acf32d0de5078
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-05-30 16:03:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: "app",
+
+  data() {
+
+
+  },
+
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppMain></AppMain>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    name: 'AppMain',
+
+    data() {
+
+    }
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>Progetti</h1>
+    </div>
+</template>
+
+<style scoped></style>
