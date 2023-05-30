b412d4019c014d16efadb0892696ecf529acef0a
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-05-30 18:02:37 +0200

    feat. api and frontend

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,72 @@
 <script>
+import axios from 'axios';
+
+import ProjectItem from './ProjectItem.vue';
 
 export default {
     name: 'AppMain',
 
     data() {
+        return {
 
+            apuURL: 'http://127.0.0.1:8000/api/projects',
+
+            projects: [],
+
+            pagination: {},
+        }
+    },
+
+    components: {
+        ProjectItem,
+    },
+
+    mounted() {
+        this.getProjects(this.apuURL);
+    },
+
+    methods: {
+        getProjects(apuURL) {
+            axios.get(apuURL).then(response => {
+                console.log(response.data.result);
+
+                this.projects = response.data.result.data;
+
+                this.pagination = response.data.result;
+            });
+        },
     }
 }
 
 </script>
 
 <template>
-    <div class="container">
+    <div class="container py-4 mb-3">
         <h1>Progetti</h1>
+
+        <div class="row">
+            <div v-for="project in projects" class="col-md-6 col-lg-4 mb-3">
+                <ProjectItem :project="project"></ProjectItem>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="project-nav">
+            <button v-for="link in pagination.links" class="btn"
+                :class="link.active ? 'btn-primary' : 'btn-outline-secondary'" v-html="link.label"
+                :disabled="link.url == null ? true : false" @click="getProjects(link.url)">
+
+            </button>
+        </div>
+
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.project-nav {
+    display: flex;
+    gap: 5px;
+    justify-content: center;
+}
+</style>
---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,103 @@
+<script>
+
+export default {
+    name: 'ProjectItem',
+
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        project: Object,
+    },
+
+    computed: {
+        content() {
+            if (this.project.content.lenght > 30) {
+                return this.project.content.substring(0, 30) + '...';
+            } else {
+                return this.project.content;
+            }
+        },
+
+        coverImage() {
+            if (this.project.cover_image) {
+                return 'http://127.0.0.1:8000/storage/' + this.project.cover_image;
+            } else {
+                return 'https://pariwisatabengkulu.com/assets/images/no-image.png';
+            }
+        },
+
+        type() {
+            if (this.project.type) {
+                return this.project.type.name;
+            } else {
+                return 'Non specificata';
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="cover-card">
+            <img :src="coverImage" alt="copertina">
+        </div>
+
+        <div class="text-card">
+            <strong>
+                {{ project.title }}
+            </strong>
+            <div>
+                {{ type }}
+            </div>
+            <div class="technologies">
+                <span v-for="technology in project.technologies" class="badge text-bg-primary">{{ technology.name }}</span>
+            </div>
+
+            <p>
+                {{ content }}
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+$titleColor: blue;
+
+.card {
+    border: 1px solid red;
+    overflow: hidden;
+
+    .text-card {
+        padding: 4px;
+
+        strong {
+            color: $titleColor;
+        }
+
+        .technologies {
+            display: flex;
+            gap: .5em;
+            margin-bottom: .5em;
+            margin-top: .5em;
+        }
+
+    }
+
+    .cover-card {
+        img {
+            height: 100px;
+            width: 100%;
+            object-fit: cover;
+            object-position: center;
+        }
+    }
+
+}
+</style>
